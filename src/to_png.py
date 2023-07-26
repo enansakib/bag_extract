bd8de26e58152b515f0a4be3cec62a13d8320ba1
Demetri <demetrious.kutzke@gmail.com>
2023-07-26 15:16:29 -0500

    Modified the script to accommodate stereo images

--- src/to_png.py
+++ src/to_png.py
@@ -8,9 +8,9 @@ Created on Thu Jul 25 12:04:34 2022
 import rospy
 import os 
 import cv2
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
-
+import message_filters
 
 
 class BagExtractor:
@@ -18,13 +18,18 @@ class BagExtractor:
     def __init__(self):
         rospy.init_node('to_png')
 
+        self.img_counter_left = 0
+        self.img_counter_right = 0
         self.img_counter = 0
-        self.out_folder = '/home/enan/TestDemetriImages'   
+        self.out_folder = '/home/dtkutzke/2023_01_30_Aqua_Raw_Images/11_24_55/'  
         os.makedirs(self.out_folder, exist_ok=True)          
         self.save_flag = False
-        # self.rate = rospy.Rate(10)
+        #self.rate = rospy.Rate(50)
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber('/stereo/right/image_raw', Image, self.imageCallBack, queue_size=3) # /stereo/right/image_raw;; /camera_front_right/image_raw
+        self.image_sub_left = message_filters.Subscriber('/camera_front_left/image_raw', Image) # /stereo/right/image_raw;; /camera_front_right/image_raw
+        self.image_sub_right = message_filters.Subscriber('/camera_front_right/image_raw', Image)
+        self.time_sync = message_filters.ApproximateTimeSynchronizer([self.image_sub_left, self.image_sub_right], queue_size=10, allow_headerless=True, slop=0.001)
+        self.time_sync.registerCallback(self.imageCallback)
 
         try:
             rospy.spin()
@@ -32,42 +37,121 @@ class BagExtractor:
             rospy.delete_param('/save_flag')
             print("Rospy shutting down.")
 
-
-    def imageCallBack(self, img_topic):
+    def imageCallback(self, img_topic_left, img_topic_right):
         try:
-            self.img_raw = self.bridge.imgmsg_to_cv2(img_topic, "bgr8")
+            self.img_raw_left = self.bridge.imgmsg_to_cv2(img_topic_left, "rgb8")
+        except CvBridgeError as e:
+            print(e)
+        try:
+            self.img_raw_right = self.bridge.imgmsg_to_cv2(img_topic_right, "rgb8")
+        except CvBridgeError as e:
+            print(e)
+                
+        if rospy.has_param('/save_flag'):
+            self.save_flag = rospy.get_param("/save_flag")
+
+        if self.img_raw_left is None or self.img_raw_right is None:
+            print('frame dropped.')
+        else:        
+            self.imageProcessor()    
+
+    def imageProcessor(self):
+        # print("test")
+
+        input_key = cv2.waitKey(1) & 0xFF
+        
+        if input_key == ord('q'):
+            rospy.delete_param('/save_flag')
+            print('Quitting the program.')
+            #cv2.destroyAllWindows()
+            rospy.signal_shutdown("Q is pressed.")
+
+
+        #cv2.imshow('Output', self.img_raw_left)        
+        if self.save_flag:
+            self.img_counter += 1
+            print("saving...")
+            cv2.imwrite(os.path.join(self.out_folder, 'img_{:03d}_left.png'.format(self.img_counter) ) , self.img_raw_left)
+            cv2.imwrite(os.path.join(self.out_folder, 'img_{:03d}_right.png'.format(self.img_counter) ) , self.img_raw_right)
+
+        #self.rate.sleep()
+
+    def imageCallBackLeft(self, img_topic):
+        try:
+            self.img_raw_left = self.bridge.imgmsg_to_cv2(img_topic, "mono8")
+            self.has_left = True
         except CvBridgeError as e:
             print(e)
         
         if rospy.has_param('/save_flag'):
             self.save_flag = rospy.get_param("/save_flag")
 
-        if self.img_raw is None:
+        if self.img_raw_left is None:
+            self.has_left = False
             print('frame dropped.')
         else:        
-            self.imageProcessor()
-            
+            self.imageProcessorLeft()
 
 
-    def imageProcessor(self):   
+
+    def imageCallBackRight(self, img_topic):
+        try:
+            self.img_raw_right = self.bridge.imgmsg_to_cv2(img_topic, "mono8")
+            self.has_right = True
+        except CvBridgeError as e:
+            print(e)
+        
+        if rospy.has_param('/save_flag'):
+            self.save_flag = rospy.get_param("/save_flag")
+
+        if self.img_raw_right is None:
+            print('frame dropped.')
+        elif self.has_left:        
+            self.imageProcessorRight()
+
+
+
+    def imageProcessorLeft(self):   
         # print("test")
 
         input_key = cv2.waitKey(1) & 0xFF
-        self.img_counter += 1
         
         if input_key == ord('q'):
             rospy.delete_param('/save_flag')
             print('Quitting the program.')
-            cv2.destroyAllWindows()
+            #cv2.destroyAllWindows()
             rospy.signal_shutdown("Q is pressed.")
 
 
-        cv2.imshow('Output', self.img_raw)        
+        #cv2.imshow('Output', self.img_raw_left)        
         if self.save_flag:
+            self.img_counter_left += 1
             print("saving.")
-            cv2.imwrite(os.path.join(self.out_folder, 'img_{:03d}.png'.format(self.img_counter) ) , self.img_raw)
+            cv2.imwrite(os.path.join(self.out_folder, 'img_{:03d}_left.png'.format(self.img_counter_left) ) , self.img_raw_left)
+
+        self.rate.sleep()
         
+    def imageProcessorRight(self):   
+        # print("test")
+
+        input_key = cv2.waitKey(1) & 0xFF
+        # self.img_counter += 1
         
+        if input_key == ord('q'):
+            rospy.delete_param('/save_flag')
+            print('Quitting the program.')
+            #cv2.destroyAllWindows()
+            rospy.signal_shutdown("Q is pressed.")
+
+
+        #cv2.imshow('Output', self.img_raw_left)        
+        if self.save_flag:
+            self.img_counter_right += 1
+            print("saving.")
+            cv2.imwrite(os.path.join(self.out_folder, 'img_{:03d}_right.png'.format(self.img_counter_right) ) , self.img_raw_right) 
+            self.has_left = False   
+
+        self.rate.sleep()
 
     
 BagExtractor()
